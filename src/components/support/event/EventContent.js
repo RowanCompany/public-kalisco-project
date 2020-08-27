a100fc7d16bea5a2980d4e3614ef1cddde41ee6a
Subin Lee <chandar.sblee@gmail.com>
2020-08-27 16:46:39 +0900

    Add load more button in support event page

--- src/components/support/event/EventContent.js
+++ src/components/support/event/EventContent.js
@@ -2,8 +2,12 @@ import React from "react";
 import EventCategoryList from "./EventCategoryList";
 import EventList from "./EventList";
 import { eventContentWrapper } from "./event.module.scss";
+import { useRouteMatch } from "react-router-dom";
 
 function EventContent() {
+    const match = useRouteMatch();
+    console.log(match);
+
     return (
         <section className={eventContentWrapper}>
             <div className="container">
--- src/components/support/event/EventList.js
+++ src/components/support/event/EventList.js
@@ -1,16 +1,33 @@
 import React from "react";
 import EventPanel from "./EventPanel";
-import { eventListWrapper } from "./event.module.scss";
+import {
+    eventListWrapper,
+    eventLoadMore,
+    eventLoadMoreWrapper,
+} from "./event.module.scss";
+import iconArrowDown from "../../../static/svg/icon-arrow-down.svg";
 
 function EventList() {
     return (
-        <div className={eventListWrapper}>
-            <EventPanel />
-            <EventPanel />
-            <EventPanel />
-            <EventPanel />
-            <EventPanel />
-        </div>
+        <>
+            <div className={eventListWrapper}>
+                <EventPanel />
+                <EventPanel />
+                <EventPanel />
+                <EventPanel />
+                <EventPanel />
+            </div>
+            <div className={eventLoadMoreWrapper}>
+                <button type="button" className={eventLoadMore}>
+                    LOAD MORE&nbsp;
+                    <img
+                        src={iconArrowDown}
+                        alt="arrow down icon"
+                        style={{ paddingLeft: "8px" }}
+                    />
+                </button>
+            </div>
+        </>
     );
 }
 
