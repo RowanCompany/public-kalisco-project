c688f61a5e807342c5412aace01bdee506396b5e
Subin Lee <chandar.sblee@gmail.com>
2020-09-01 18:16:16 +0900

    Add product sauce detail content (WIP)

--- src/components/product/ProductSauceDetail.js
+++ src/components/product/ProductSauceDetail.js
@@ -3,7 +3,7 @@ import ProductDetailNavigator from "./ProductDetailNavigator";
 import styles from "./product.module.scss";
 import { useParams, Redirect } from "react-router-dom";
 import SauceNavigator from "./SauceNavigator";
-
+import imgCurry1 from "../../static/images/img-p-sauce-curry-1.png";
 
 function ProductSauceDetail({ content }) {
     const { sauce } = useParams();
@@ -20,8 +20,16 @@ function ProductSauceDetail({ content }) {
                         <ProductDetailNavigator content={content} />
                         <SauceNavigator sauce={sauce} />
                         <div className="row">
-                            <div className="col"></div>
-                            <div className="col"></div>
+                            <div className="col">
+                                <img
+                                    src={imgCurry1}
+                                    alt="product example"
+                                    className="img-fill"
+                                />
+                            </div>
+                            <div
+                                className={`col ${styles.sauceDetailTextWrapper}`}
+                            ></div>
                         </div>
                         <div className="row">
                             <div className="col"></div>
