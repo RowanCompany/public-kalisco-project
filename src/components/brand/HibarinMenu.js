1fbf49c70101ab4708b0ab17010f507cbb548723
Subin Lee <chandar.sblee@gmail.com>
2020-09-04 11:41:38 +0900

    Add hibarin menu landing page

--- src/components/brand/HibarinMenu.js
+++ src/components/brand/HibarinMenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./hibarin.module.scss";
 import textMenuImage from "../../static/images/text-hibarin-menu.png";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -10,34 +10,36 @@ import menu4 from "../../static/images/img-p-hibarin-menu4.png";
 import menu5 from "../../static/images/img-p-hibarin-menu5.png";
 import menu6 from "../../static/images/img-p-hibarin-menu6.png";
 import textOrdinaryImage from "../../static/images/text-hibrain-ordinary.png";
+import katsuImage from "../../static/images/img-ormenu-katsu.png";
+import donburiImage from "../../static/images/img-ormenu-donburi.png";
+import noodleImage from "../../static/images/img-ormenu-noodle.png";
+import sideImage from "../../static/images/img-ormenu-side.png";
 
 import "swiper/components/navigation/navigation.scss";
 import "./swiper_menu.scss";
+import { Link } from "react-router-dom";
 
 function HibarinMenu() {
     SwiperCore.use([Navigation]);
     const [menu, setMenu] = useState("katsu");
-    const [menuSectionStyle, setMenuSectionStyle] = useState(
-        styles.katsuSection
-    );
-    // TODO: 오브젝트로 바꿔야 함
-    useEffect(() => {
-        switch (menu) {
-            case "side":
-                setMenuSectionStyle(styles.sideSection);
-                break;
-            case "noodle":
-                setMenuSectionStyle(styles.noodleSection);
-                break;
-            case "donburi":
-                setMenuSectionStyle(styles.donburiSection);
-                break;
-            case "katsu":
-            default:
-                setMenuSectionStyle(styles.katsuSection);
-                break;
-        }
-    }, [menu, menuSectionStyle]);
+    const menuData = {
+        katsu: {
+            sectionStyle: styles.katsuSection,
+            image: katsuImage,
+        },
+        donburi: {
+            sectionStyle: styles.donburiSection,
+            image: donburiImage,
+        },
+        noodle: {
+            sectionStyle: styles.noodleSection,
+            image: noodleImage,
+        },
+        side: {
+            sectionStyle: styles.sideSection,
+            image: sideImage,
+        },
+    };
 
     return (
         <>
@@ -112,10 +114,97 @@ function HibarinMenu() {
                 </div>
             </section>
             <section
-                className={`${styles.menuListSection} ${menuSectionStyle}`}
+                className={`${styles.menuListSection} ${
+                    menuData[menu] && menuData[menu]["sectionStyle"]
+                }`}
             >
-                <div className="text-center">
+                <div className={styles.menuListTitleWrapper}>
                     <img src={textOrdinaryImage} alt="ordinary menu" />
+                </div>
+                <div className={`row ${styles.menuListGridWrapper}`}>
+                    <div className="col">
+                        <div className={styles.MenuListFlexWrapper}>
+                            <div
+                                className={`${styles.menuBorderLine} ${styles.katsuBorderLine}`}
+                            />
+                            <Link
+                                onMouseEnter={() => setMenu("katsu")}
+                                className={styles.menuListLinkWrapper}
+                            >
+                                <div
+                                    className={`${styles.menuListTitle} ${
+                                        menu === "katsu" &&
+                                        styles.menuListTitleActive
+                                    }`}
+                                >
+                                    Katsu
+                                </div>
+                            </Link>
+                        </div>
+                        <div className={styles.MenuListFlexWrapper}>
+                            <div
+                                className={`${styles.menuBorderLine} ${styles.donburiBorderLine}`}
+                            />
+                            <Link
+                                onMouseEnter={() => setMenu("donburi")}
+                                className={styles.menuListLinkWrapper}
+                            >
+                                <div
+                                    className={`${styles.menuListTitle} ${
+                                        menu === "donburi" &&
+                                        styles.menuListTitleActive
+                                    }`}
+                                >
+                                    Donburi
+                                </div>
+                            </Link>
+                        </div>
+                        <div className={styles.MenuListFlexWrapper}>
+                            <div
+                                className={`${styles.menuBorderLine} ${styles.noodleBorderLine}`}
+                            />
+                            <Link
+                                onMouseEnter={() => setMenu("noodle")}
+                                className={styles.menuListLinkWrapper}
+                            >
+                                <div
+                                    className={`${styles.menuListTitle} ${
+                                        menu === "noodle" &&
+                                        styles.menuListTitleActive
+                                    }`}
+                                >
+                                    Noodle
+                                </div>
+                            </Link>
+                        </div>
+                        <div className={styles.MenuListFlexWrapper}>
+                            <div
+                                className={`${styles.menuBorderLine} ${styles.sideBorderLine}`}
+                            />
+                            <Link
+                                onMouseEnter={() => setMenu("side")}
+                                className={styles.menuListLinkWrapper}
+                            >
+                                <div
+                                    className={`${styles.menuListTitle} ${
+                                        menu === "side" &&
+                                        styles.menuListTitleActive
+                                    }`}
+                                >
+                                    Side
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="text-center">
+                            <img
+                                src={menuData[menu] && menuData[menu]["image"]}
+                                alt="menu"
+                                className="img-fluid"
+                            />
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
