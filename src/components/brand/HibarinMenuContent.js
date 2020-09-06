5c2ba59f036888157c27312e99012562a2a676a1
Subin Lee <chandar.sblee@gmail.com>
2020-09-07 01:16:39 +0900

    Add hibarin menu detail panels

--- src/components/brand/HibarinMenuContent.js
+++ src/components/brand/HibarinMenuContent.js
@@ -5,61 +5,61 @@ import HibarinBanner from "./HibarinBanner";
 import { useParams } from "react-router-dom";
 import CategoryLink from "../categoryButton/CategoryLink";
 import HibarnMenuData from "./HibarinMenuData";
+import MenuPanel from "./panel/MenuPanel";
 
 function HibarinMenuContent() {
-    const params = useParams();
-    const categoryData = [
-        {
-            text: "Katsu",
-            type: "katsu",
-            link: "/brand/hibarin/menu/katsu",
-        },
-        {
-            text: "Donburi",
-            type: "donburi",
-            link: "/brand/hibarin/menu/donburi",
-        },
-        {
-            text: "Noodles",
-            type: "noodles",
-            link: "/brand/hibarin/menu/noodles",
-        },
-        {
-            text: "Side",
-            type: "side",
-            link: "/brand/hibarin/menu/side",
-        },
-    ];
-    const [category, setCategory] = useState(
-        params.menu ? params.menu : "katsu"
-    );
+  const params = useParams();
+  const categoryData = [
+    {
+      text: "Katsu",
+      type: "katsu",
+      link: "/brand/hibarin/menu/katsu",
+    },
+    {
+      text: "Donburi",
+      type: "donburi",
+      link: "/brand/hibarin/menu/donburi",
+    },
+    {
+      text: "Noodles",
+      type: "noodles",
+      link: "/brand/hibarin/menu/noodles",
+    },
+    {
+      text: "Side",
+      type: "side",
+      link: "/brand/hibarin/menu/side",
+    },
+  ];
+  const [category, setCategory] = useState(params.menu ? params.menu : "katsu");
 
-    return (
-        <>
-            <HibarinBanner subject={params.subject} />
-            <section className={styles.menuDetailSection}>
-                <div className={styles.categoryButtonListWrapper}>
-                    {categoryData.map((d, i) => (
-                        <CategoryLink
-                            key={i}
-                            {...d}
-                            parentType={category}
-                            clickEvent={() => setCategory(d.type)}
-                        />
-                    ))}
-                </div>
-                <div className="container">
-                    {HibarnMenuData[category] &&
-                        HibarnMenuData[category].map((d, i) => (
-                            <div key={i}>
-                                <div>hi</div>
-                            </div>
-                        ))}
-                </div>
-            </section>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <HibarinBanner subject={params.subject} />
+      <section className={styles.menuDetailSection}>
+        <div className={styles.categoryButtonListWrapper}>
+          {categoryData.map((d, i) => (
+            <CategoryLink
+              key={i}
+              {...d}
+              parentType={category}
+              clickEvent={() => setCategory(d.type)}
+            />
+          ))}
+        </div>
+        {/* TODO: Modal 제작해야 함 */}
+        <div className="container">
+          <div className={styles.menuDetailPanelWrapper}>
+            {HibarnMenuData[category] &&
+              HibarnMenuData[category].map((d, i) => (
+                <MenuPanel key={i} {...d} />
+              ))}
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
 
 export default HibarinMenuContent;
--- src/components/brand/panel/MenuPanel.js
+++ src/components/brand/panel/MenuPanel.js
@@ -1,11 +1,24 @@
 import React from "react";
+import styles from "./menu_panel.module.scss";
 
-function MenuPanel() {
-    return (
-        <div>
-            <div></div>
+// TODO: 팝업 해야 함
+function MenuPanel(props) {
+  return (
+    <div className={styles.menuPanel}>
+      <div>
+        <img src={props.image} alt={props.title} className="img-fill" />
+      </div>
+      <div className={styles.panelTextWrapper}>
+        <div className={styles.panelFlexWrapper}>
+          <div className={styles.panelTitleWrapper}>
+            <div className={styles.panelTitle}>{props.title}</div>
+            <div className={styles.panelEnTitle}>{props.enTitle}</div>
+          </div>
+          <div className={styles.panelPrice}>{props.price}</div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default MenuPanel;
