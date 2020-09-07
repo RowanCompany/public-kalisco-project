1dae11bdbd5d20d85e6b26942d9a9879e8c6a9e7
Subin Lee <chandar.sblee@gmail.com>
2020-09-07 16:51:58 +0900

    Add hibarin menu data

--- src/components/brand/HibarinMenuData.js
+++ src/components/brand/HibarinMenuData.js
@@ -13,6 +13,38 @@ import katsu12 from "../../static/images/menu-hibarin-kastu-12.png";
 import katsu13 from "../../static/images/menu-hibarin-kastu-13.png";
 import katsu14 from "../../static/images/menu-hibarin-kastu-14.png";
 import katsu15 from "../../static/images/menu-hibarin-kastu-15.png";
+import donburi1 from "../../static/images/menu-hibarin-donburi-1.jpg";
+import donburi2 from "../../static/images/menu-hibarin-donburi-2.jpg";
+import donburi3 from "../../static/images/menu-hibarin-donburi-3.jpg";
+import donburi4 from "../../static/images/menu-hibarin-donburi-4.jpg";
+import donburi5 from "../../static/images/menu-hibarin-donburi-5.jpg";
+import donburi6 from "../../static/images/menu-hibarin-donburi-6.jpg";
+import donburi7 from "../../static/images/menu-hibarin-donburi-7.jpg";
+import donburi8 from "../../static/images/menu-hibarin-donburi-8.jpg";
+import donburi9 from "../../static/images/menu-hibarin-donburi-9.jpg";
+import noodles1 from "../../static/images/menu-hibarin-noodles-1.jpg";
+import noodles2 from "../../static/images/menu-hibarin-noodles-2.jpg";
+import noodles3 from "../../static/images/menu-hibarin-noodles-3.jpg";
+import noodles4 from "../../static/images/menu-hibarin-noodles-4.jpg";
+import noodles5 from "../../static/images/menu-hibarin-noodles-5.jpg";
+import noodles6 from "../../static/images/menu-hibarin-noodles-6.jpg";
+import noodles7 from "../../static/images/menu-hibarin-noodles-7.jpg";
+import noodles8 from "../../static/images/menu-hibarin-noodles-8.jpg";
+import noodles9 from "../../static/images/menu-hibarin-noodles-9.jpg";
+import noodles10 from "../../static/images/menu-hibarin-noodles-10.jpg";
+import side1 from "../../static/images/menu-hibarin-side-1.jpg";
+import side2 from "../../static/images/menu-hibarin-side-2.jpg";
+import side3 from "../../static/images/menu-hibarin-side-3.jpg";
+import side4 from "../../static/images/menu-hibarin-side-4.jpg";
+import side5 from "../../static/images/menu-hibarin-side-5.jpg";
+import side6 from "../../static/images/menu-hibarin-side-6.jpg";
+import side7 from "../../static/images/menu-hibarin-side-7.jpg";
+import side8 from "../../static/images/menu-hibarin-side-8.jpg";
+import side9 from "../../static/images/menu-hibarin-side-9.jpg";
+import side10 from "../../static/images/menu-hibarin-side-10.jpg";
+import side11 from "../../static/images/menu-hibarin-side-11.jpg";
+import side12 from "../../static/images/menu-hibarin-side-12.jpg";
+import side13 from "../../static/images/menu-hibarin-side-13.jpg";
 
 export default {
     katsu: [
@@ -53,35 +85,310 @@ export default {
             description: "#치즈매니아 #부드럽고진한 #자연치즈",
             price: "14.0",
         },
-        // {
-        //   image: katsu6,
-        // },
-        // {
-        //   image: katsu7,
-        // },
-        // {
-        //   image: katsu8,
-        // },
-        // {
-        //   image: katsu9,
-        // },
-        // {
-        //   image: katsu10,
-        // },
-        // {
-        //   image: katsu11,
-        // },
-        // {
-        //   image: katsu12,
-        // },
-        // {
-        //   image: katsu13,
-        // },
-        // {
-        //   image: katsu14,
-        // },
-        // {
-        //   image: katsu15,
-        // },
+        {
+            image: katsu6,
+            title: "명란치즈카츠",
+            enTitle: "cod roe & cheese katsu",
+            description:
+                "#모짜렐라 #명란 #꽈리고추튀김 #한입스타일 #술안주굿 #포토제닉",
+            price: "16.0",
+        },
+        {
+            image: katsu7,
+            title: "콰트로 치즈 카츠",
+            enTitle: "Quatro cheese katsu",
+            description:
+                "#4가지치즈 #치즈홀릭 #모짜렐라 #체다 #고르곤졸라 #그라나파다노",
+            price: "16.0",
+        },
+        {
+            image: katsu8,
+            title: "씨푸드 카츠",
+            enTitle: "assorted seafood katsu set",
+            description: "#통새우 새우살스테이크 흰살생선카츠 #매콤크림",
+            price: "16.0",
+        },
+        {
+            image: katsu9,
+            title: "비프함박카츠",
+            enTitle: "beef hamburg katsu",
+            description: "#소고기를가득품은 #겉바속촉 #고소한수란",
+            price: "16.0",
+        },
+        {
+            image: katsu10,
+            title: "키즈카츠",
+            enTitle: "kid katsu with udon and katsu(shrimp/cheese)",
+            description: "#미니카츠 #우동 #주먹밥 #새우 #치즈",
+            price: "9.0",
+        },
+        {
+            image: katsu11,
+            title: "bone-in 갈비 카츠",
+            enTitle: "bone-in steak katsu",
+            description:
+                "#t-bone이라 #안심등심 같이 먹는 #초대형 #300g #육즙풍부 #스페셜카츠 #진정한 #고기맛 #겉바속촉 #히노끼에이징 #수퍼사이즈 #signature #최상의 맛",
+            price: "30.0",
+        },
+        {
+            image: katsu12,
+            title: "블랙 카츠(히레/로스)",
+            enTitle: "black katsu(tenderloin/sirloin)",
+            description:
+                "#오징어먹물 #블랙카카오 #블랙빵가루 #고소한 #평범한것먹기싫은당신",
+            price: "16.0 / 15.0",
+        },
+        {
+            image: katsu13,
+            title: "마늘치즈 24겹 카츠",
+            enTitle: "24 layer pork katsu \nwith cheese and garlic",
+            description: "#24겹 #사이사이마늘 #치즈 #고소",
+            price: "15.5",
+        },
+        {
+            image: katsu14,
+            title: "모짜체다 치즈 카츠",
+            enTitle: "mozza cheddar cheese katsu",
+            description:
+                "#모짜렐라 #체다치즈 #2가지치즈 #치즈매니아 #부드럽고 진한",
+            price: "15.0",
+        },
+        {
+            image: katsu15,
+            title: "테판24겹카츠",
+            enTitle: "original 24layer pork katsu",
+            descirption: "#24겹 #바삭촉촉 #지글지글 #태판 #뜨거운 #양배추",
+            price: "15.0",
+        },
+    ],
+    donburi: [
+        {
+            image: donburi1,
+            title: "아보카도 연어 라이스",
+            enTitle: "avocado+salmon rice bowl",
+            descripton: "#아보카도 #슈퍼푸드 #덮밥 #연어회 #와사비 #건강",
+            price: "15.0",
+        },
+        {
+            image: donburi2,
+            title: "숯불 불고기 핫팟",
+            enTitle: "charcoal-grilled bulgogi hot pot",
+            descripton: "#숯불불고기 #돌솥 #덮밥",
+            price: "15.0",
+        },
+        {
+            image: donburi3,
+            title: "카츠동",
+            enTitle: "katsu rice bowl",
+            descripton: "#계란 #카츠 #특제소스 #덮밥 #카츠동",
+            price: "13.0",
+        },
+        {
+            image: donburi4,
+            title: "카레라이스 + 카츠 or 새우",
+            enTitle: "curry rice + katsu or shrimp",
+            descripton: "#바삭한 카츠 #통새우를 얹은 #카레라이스",
+            price: "12.0",
+        },
+        {
+            image: donburi5,
+            title: "24겹 김치 카츠 마끼",
+            enTitle: "24-layer kimchi katsu maki",
+            descripton: "#24겹카사네 #바삭촉촉 #사이사이김치",
+            price: "11.0",
+        },
+        {
+            image: donburi6,
+            title: "김치 카츠동",
+            enTitle: "kimchi katsu rice bowl",
+            descripton: "#부드러운 돈카츠 #계란 #아삭한김치 #완벽조화",
+            price: "13.5",
+        },
+        {
+            image: donburi7,
+            title: "에비동",
+            enTitle: "shrimp rice bowl",
+            descripton: "#통새우카츠 #덮밥 #에비동 #씨푸드매니아",
+            price: "13.0",
+        },
+        {
+            image: donburi8,
+            title: "명란알밥",
+            enTitle: "rice with mentaike and fish roe",
+            descripton: "#명란듬뿍 #산뜻한한끼 #히바린특제알밥",
+            price: "13.5",
+        },
+        {
+            image: donburi9,
+            title: "와사비 슈림프 마끼",
+            enTitle: "wasabi shrimp maki",
+            descripton: "#통새우 #마끼 #크레이지롤 #와사비 #오이",
+            price: "12.0",
+        },
+    ],
+    noodles: [
+        {
+            image: noodles1,
+            title: "육개장 불고기 누들",
+            enTitle: "spicy beef noodle with bulgogi",
+            descripton: "#얼큰불고기 누들 #칼칼 #개운 #국물이 #끝내줘요",
+            price: "13.0",
+        },
+        {
+            image: noodles2,
+            title: "숯불 불고기 우동",
+            enTitle: "charcoal-grilled bulgogi udon",
+            descripton: "#불고기듬뿍 #숯불향 #가쓰오부시 국물 #든든함",
+            price: "14.0",
+        },
+        {
+            image: noodles3,
+            title: "야채 가라아게 우동",
+            enTitle: "Vegetable tempura udon",
+            descripton: "#야채튀김듬뿍 #바삭바삭 #국물 #담백",
+            price: "11.0",
+        },
+        {
+            image: noodles4,
+            title: "간사이 소바",
+            enTitle: "mori soba noodle",
+            descripton: "#찍먹 #쯔유 #소바 #시원쫄깃",
+            price: "11.0",
+        },
+        {
+            image: noodles5,
+            title: "명란 크림 우동",
+            enTitle: "mentaiko cream udon",
+            descripton: "#크림소스 #명란 #모던일식파스타 #묘한중독",
+            price: "13.0",
+        },
+        {
+            image: noodles6,
+            title: "히야시 소바",
+            enTitle: "cool soba noodle",
+            descripton: "#냉소바 #메밀면 #시원쫄깃 #여름",
+            price: "12.0",
+        },
+        {
+            image: noodles7,
+            title: "카레 우동 + 카츠 or 새우",
+            enTitle: "curry udon+katsu or shrimp",
+            descripton: "#바삭한 카츠 #통새우를 얹은 #카레우동",
+            price: "12.0",
+        },
+        {
+            image: noodles8,
+            title: "샐러드 소바",
+            enTitle: "salad soba",
+            descripton: "#어린잎채소 #소바면 #레몬참깨드레싱",
+            price: "8.0",
+        },
+        {
+            image: noodles9,
+            title: "치킨카츠냉소바",
+            enTitle: "cold chicken soba with lemon",
+            descripton: "#시원상큼한소바 #바삭한치킨카츠",
+            price: "15.0",
+        },
+        {
+            image: noodles10,
+            title: "청양불고기비빔소바",
+            enTitle: "spicy bulgogi soba",
+            descripton: "#매콤고소 특제청양소스 #비빔소바 #숯불향기",
+            price: "15.0",
+        },
+    ],
+    side: [
+        {
+            image: side1,
+            title: "치즈 아란치니(2조각)",
+            enTitle: "cheese arancini(2pcs)",
+            descripton: "",
+            price: "5.0",
+        },
+        {
+            image: side2,
+            title: "미니 카츠(2조각)",
+            enTitle: "mini katsu(2pcs)",
+            descripton: "",
+            price: "3.5",
+        },
+        {
+            image: side3,
+            title: "1/2 로스 카츠",
+            enTitle: "half roast katsu",
+            descripton: "",
+            price: "5.5",
+        },
+        {
+            image: side4,
+            title: "모짜렐라치즈 카츠(2조각)",
+            enTitle: "mozzarella cheese katsu(2pcs)",
+            descripton: "",
+            price: "3.5",
+        },
+        {
+            image: side5,
+            title: "생선 카츠(2조각)",
+            enTitle: "sea food katsu(2pcs)",
+            descripton: "",
+            price: "3.5",
+        },
+        {
+            image: side6,
+            title: "통새우 카츠(1조각/2조각)",
+            enTitle: "shrimp katsu(1pc / 2pcs)",
+            descripton: "",
+            price: "3.5 / 5.0",
+        },
+        {
+            image: side7,
+            title: "안심미트볼카츠(3조각)",
+            enTitle: "tenderioin meet ball katsu(3pcs)",
+            descripton: "",
+            price: "3.5",
+        },
+        {
+            image: side8,
+            title: "고로케(카레/크림치즈)",
+            enTitle: "croquette(curry/creem cheese)",
+            descripton: "",
+            price: "3.5 / 4.5",
+        },
+        {
+            image: side9,
+            title: "미니소바",
+            enTitle: "mini soba",
+            descripton: "",
+            price: "3.8",
+        },
+        {
+            image: side10,
+            title: "미니우동",
+            enTitle: "mini udon",
+            descripton: "",
+            price: "3.8",
+        },
+        {
+            image: side11,
+            title: "소바 추가(면/장)",
+            enTitle: "added soba(noodle/soup)",
+            descripton: "",
+            price: "5.0",
+        },
+        {
+            image: side12,
+            title: "카레소스",
+            enTitle: "curry sauce",
+            descripton: "",
+            price: "2.0",
+        },
+        {
+            image: side13,
+            title: "밥",
+            enTitle: "rice",
+            descripton: "",
+            price: "2.5",
+        },
     ],
 };
--- src/components/brand/panel/MenuPanel.js
+++ src/components/brand/panel/MenuPanel.js
@@ -3,22 +3,24 @@ import styles from "./menu_panel.module.scss";
 
 // TODO: 팝업 해야 함
 function MenuPanel(props) {
-  return (
-    <div className={styles.menuPanel}>
-      <div>
-        <img src={props.image} alt={props.title} className="img-fill" />
-      </div>
-      <div className={styles.panelTextWrapper}>
-        <div className={styles.panelFlexWrapper}>
-          <div className={styles.panelTitleWrapper}>
-            <div className={styles.panelTitle}>{props.title}</div>
-            <div className={styles.panelEnTitle}>{props.enTitle}</div>
-          </div>
-          <div className={styles.panelPrice}>{props.price}</div>
+    return (
+        <div className={styles.menuPanel}>
+            <div>
+                <img src={props.image} alt={props.title} className="img-fill" />
+            </div>
+            <div className={styles.panelTextWrapper}>
+                <div className={styles.panelFlexWrapper}>
+                    <div className={styles.panelTitleWrapper}>
+                        <div className={styles.panelTitle}>{props.title}</div>
+                        <div className={styles.panelEnTitle}>
+                            {props.enTitle}
+                        </div>
+                    </div>
+                    <div className={styles.panelPrice}>{props.price}</div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default MenuPanel;
