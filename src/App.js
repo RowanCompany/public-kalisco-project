f8bcef34e1394f7dfd0c3179aaf0ab41598ab8ab
Subin Lee <chandar.sblee@gmail.com>
2020-09-09 09:52:58 +0900

    Add scroll to top when every page transitions

--- src/App.js
+++ src/App.js
@@ -7,23 +7,27 @@ import Brand from "./pages/brand/Brand";
 import Product from "./pages/product/Product";
 import Recruit from "./pages/recruit/Recruit";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 // TODO: Suspense 도입 고려
 function App() {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/about" component={About} />
-                <Route path="/supports" component={Supports} />
-                <Route path="/recruit" component={Recruit} />
-                <Route path="/hr" component={HR} />
-                <Route path="/brand" component={Brand} />
-                <Route path="/products" component={Product} />
-                <Route>
-                    <Redirect to="/" />
-                </Route>
-            </Switch>
+            <>
+                <ScrollToTop />
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/about" component={About} />
+                    <Route path="/supports" component={Supports} />
+                    <Route path="/recruit" component={Recruit} />
+                    <Route path="/hr" component={HR} />
+                    <Route path="/brand" component={Brand} />
+                    <Route path="/products" component={Product} />
+                    <Route>
+                        <Redirect to="/" />
+                    </Route>
+                </Switch>
+            </>
         </BrowserRouter>
     );
 }
