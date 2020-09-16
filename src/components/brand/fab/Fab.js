7835fa469554f4893bf42a2fd94f5aa8532eea98
Subin Lee <chandar.sblee@gmail.com>
2020-09-16 12:17:28 +0900

    Add link in fab component

--- src/components/brand/fab/Fab.js
+++ src/components/brand/fab/Fab.js
@@ -3,19 +3,51 @@ import styles from "./fab.module.scss";
 import iconInstargram from "../../../static/svg/icon_insta.svg";
 import iconFacebook from "../../../static/svg/icon_facebook.svg";
 import iconSupport from "../../../static/svg/fab_support.svg";
+import { Link } from "react-router-dom";
 
-function Fab() {
+function Fab({ subject }) {
+    const data = {
+        saboten: {
+            facebook: "https://www.facebook.com/sabotenkorea",
+            instagram: "https://www.instagram.com/saboten_korea/",
+        },
+        hibarin: {
+            facebook: "https://www.facebook.com/hibarinkr",
+            instagram: "https://www.instagram.com/hibarin_korea/",
+        },
+    };
+
     return (
         <div className={styles.fabWrapper}>
-            <div className={styles.supportIcon}>
-                <img src={iconSupport} alt="instagram" />
-            </div>
-            <div className={styles.facebookIcon}>
-                <img src={iconFacebook} alt="instagram" />
-            </div>
-            <div className={styles.instagramIcon}>
-                <img src={iconInstargram} alt="instagram" />
-            </div>
+            <Link to={`/contact?subject=${subject}`} className="d-inline-block">
+                <div className={styles.supportIcon}>
+                    <img src={iconSupport} alt="instagram" />
+                </div>
+            </Link>
+            {data[subject] && (
+                <>
+                    <a
+                        href={data[subject]["facebook"]}
+                        rel="noopener noreferrer"
+                        className="d-block"
+                        target="_blank"
+                    >
+                        <div className={styles.facebookIcon}>
+                            <img src={iconFacebook} alt="instagram" />
+                        </div>
+                    </a>
+                    <a
+                        href={data[subject]["instagram"]}
+                        rel="noopener noreferrer"
+                        className="d-block"
+                        target="_blank"
+                    >
+                        <div className={styles.instagramIcon}>
+                            <img src={iconInstargram} alt="instagram" />
+                        </div>
+                    </a>
+                </>
+            )}
         </div>
     );
 }
