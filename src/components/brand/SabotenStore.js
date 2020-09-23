d44f9d6a3d37a7dbe835a8b4e7e89dc1bd1e09a9
Subin Lee <chandar.sblee@gmail.com>
2020-09-23 11:36:24 +0900

    Add marker on saboten store page

--- src/components/brand/SabotenStore.js
+++ src/components/brand/SabotenStore.js
@@ -3,8 +3,9 @@ import storeText from "../../static/images/text-hibarin-store.png";
 import styles from "./hibarin.module.scss";
 import masanStoreImage from "../../static/images/store-masan.png";
 import newredStoreImage from "../../static/images/store-newred.png";
-import mapImage from "../../static/images/img-p-store-map-korea.png";
+import mapImage from "../../static/images/img-map-korea.png";
 import markerImage from "../../static/images/img-p-store-map-marker.png";
+import markerOnMapImage from "../../static/images/icon-map-marker.png";
 
 function SabotenStore() {
     return (
@@ -88,11 +89,76 @@ function SabotenStore() {
                             </div>
                         </div>
                         <div>
-                            <img
-                                src={mapImage}
-                                alt="korea map"
-                                className="img-fluid"
-                            />
+                            <div className={styles.storeMapImageWrapper}>
+                                <a
+                                    href="https://map.naver.com/v5/search/%EC%82%AC%EB%B3%B4%ED%85%90%20%EA%B0%95%EB%82%A8?c=14145111.5970845,4510988.5520209,12,0,0,0,dh"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className={styles.storeMapMarkerPosition}
+                                    style={{ top: "95px", left: "85px" }}
+                                >
+                                    <img src={markerOnMapImage} alt="marker" />
+                                </a>
+                                <a
+                                    href="https://map.naver.com/v5/search/%EC%82%AC%EB%B3%B4%ED%85%90%20%EC%8A%A4%ED%83%80%ED%95%84%EB%93%9C%EA%B3%A0%EC%96%91?c=14125842.2999852,4529648.7415510,15,0,0,0,dh"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className={styles.storeMapMarkerPosition}
+                                    style={{ top: "90px", left: "50px" }}
+                                >
+                                    <img src={markerOnMapImage} alt="marker" />
+                                </a>
+                                <a
+                                    href="https://map.naver.com/v5/search/%EC%82%AC%EB%B3%B4%ED%85%90%20%EC%9D%B8%EC%B2%9C?c=14086694.5982175,4500459.3513746,11,0,0,0,dh"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className={styles.storeMapMarkerPosition}
+                                    style={{ top: "140px", left: "70px" }}
+                                >
+                                    <img src={markerOnMapImage} alt="marker" />
+                                </a>
+                                <a
+                                    href="https://map.naver.com/v5/search/%EC%82%AC%EB%B3%B4%ED%85%90%20%EC%B6%A9%EB%82%A8?c=14115626.0134235,4396734.3039916,9,0,0,0,dh"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className={styles.storeMapMarkerPosition}
+                                    style={{ top: "180px", left: "40px" }}
+                                >
+                                    <img src={markerOnMapImage} alt="marker" />
+                                </a>
+                                <a
+                                    href="https://map.naver.com/v5/search/%EC%82%AC%EB%B3%B4%ED%85%90%20%EB%8C%80%EC%A0%84?c=14165539.3928937,4348922.9427992,10,0,0,0,dh"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className={styles.storeMapMarkerPosition}
+                                    style={{ top: "230px", left: "115px" }}
+                                >
+                                    <img src={markerOnMapImage} alt="marker" />
+                                </a>
+                                <a
+                                    href="https://map.naver.com/v5/search/%EC%82%AC%EB%B3%B4%ED%85%90%20%EA%B4%91%EC%A3%BC?c=14123365.2625373,4187583.4853463,12,0,0,0,dh"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className={styles.storeMapMarkerPosition}
+                                    style={{ top: "320px", left: "100px" }}
+                                >
+                                    <img src={markerOnMapImage} alt="marker" />
+                                </a>
+                                <a
+                                    href="https://map.naver.com/v5/search/%EC%82%AC%EB%B3%B4%ED%85%90%20%EC%B0%BD%EC%9B%90?c=14322502.8304773,4193335.3717248,11,0,0,0,dh"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    className={styles.storeMapMarkerPosition}
+                                    style={{ top: "360px", right: "70px" }}
+                                >
+                                    <img src={markerOnMapImage} alt="marker" />
+                                </a>
+                                <img
+                                    src={mapImage}
+                                    alt="korea map"
+                                    className="img-fluid"
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
