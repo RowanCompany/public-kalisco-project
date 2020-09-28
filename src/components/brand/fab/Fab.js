ccdf58ba4d30985825489a2530833464d203404f
Subin Lee <chandar.sblee@gmail.com>
2020-09-28 10:23:43 +0900

    Add map placeholder fab in brand pages

--- src/components/brand/fab/Fab.js
+++ src/components/brand/fab/Fab.js
@@ -3,6 +3,7 @@ import styles from "./fab.module.scss";
 import iconInstargram from "../../../static/svg/icon_insta.svg";
 import iconFacebook from "../../../static/svg/icon_facebook.svg";
 import iconSupport from "../../../static/svg/fab_support.svg";
+import iconPlaceholder from "../../../static/svg/icon-placeholder.svg";
 import { Link } from "react-router-dom";
 
 function Fab({ subject }) {
@@ -10,15 +11,31 @@ function Fab({ subject }) {
         saboten: {
             facebook: "https://www.facebook.com/sabotenkorea",
             instagram: "https://www.instagram.com/saboten_korea/",
+            map:
+                "https://map.naver.com/v5/search/%EC%82%AC%EB%B3%B4%ED%85%90?placeSearchOption=fromNxList=true%26noredirect=1%26entry=pll&c=14135681.1787198,4517242.0563975,13,0,0,0,dh",
         },
         hibarin: {
             facebook: "https://www.facebook.com/hibarinkr",
             instagram: "https://www.instagram.com/hibarin_korea/",
+            map:
+                "https://map.naver.com/v5/search/%ED%9E%88%EB%B0%94%EB%A6%B0?placeSearchOption=fromNxList=true%26noredirect=1%26entry=pll&c=14135529.7255359,4513992.1748852,13,0,0,0,dh",
         },
     };
 
     return (
         <div className={styles.fabWrapper}>
+            {data[subject] && (
+                <a
+                    className="d-block"
+                    href={data[subject]["map"]}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <div className={styles.mapPlaceholderIcon}>
+                        <img src={iconPlaceholder} alt="placeholder" />
+                    </div>
+                </a>
+            )}
             <Link to={`/contact?subject=${subject}`} className="d-inline-block">
                 <div className={styles.supportIcon}>
                     <img src={iconSupport} alt="instagram" />
