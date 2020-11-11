f3ab0e0b3af02a47475f6f13173c23f27964a3f9
Subin Lee <chandar.sblee@gmail.com>
2020-11-11 20:19:39 +0900

    WIP mobile recruit page

--- src/pages/recruit/Recruit.js
+++ src/pages/recruit/Recruit.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import Nav from "../../components/nav/Nav";
 import Footer from "../../components/Footer";
 import RecruitComponent from "../../components/recruit/Recruit";
 import Fab from "../../components/brand/fab/Fab";
+import { useMobileCheck } from "../../utils/mobile";
+
+function DesktopRecruitComponent() {
+  return (
+    <>
+      <Nav />
+      <RecruitComponent />
+      <Footer />
+      <Fab />
+    </>
+  );
+}
 
 function Recruit() {
-    return (
-        <>
-            <Nav />
-            <RecruitComponent />
-            <Footer />
-            <Fab />
-        </>
-    );
+  const [width, setWidth] = useState(window.innerWidth);
+  useLayoutEffect(() => {
+    window.addEventListener("resize", (e) => {
+      setWidth(e.target.innerWidth);
+    });
+
+    return () => {
+      window.addEventListener("resize", (e) => {
+        setWidth(e.target.innerWidth);
+      });
+    };
+  }, []);
+  return useMobileCheck(width) ? <DesktopRecruitComponent /> : "";
 }
 
 export default Recruit;
