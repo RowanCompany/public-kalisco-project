5f2801764c73e05c028eeea71077ed6f1137c959
Subin Lee <chandar.sblee@gmail.com>
2020-11-20 22:04:54 +0900

    메인 모바일 페이지 푸터 수정

--- src/components/mobile/mainVerticalList/MobileMainVerticalList.js
+++ src/components/mobile/mainVerticalList/MobileMainVerticalList.js
@@ -262,7 +262,7 @@ export default function MobileMainVerticalList({ rootHeight }) {
                 text={<>Recruit</>}
               />
             </div>
-            <div className={styles.lastSlideFooterWrapper}>
+            {/*<div className={styles.lastSlideFooterWrapper}>
               <div className={styles.footerLogoWrapper}>
                 <img src={kaliscoWhiteLogo} alt="kalisco white logo" />
               </div>
@@ -271,7 +271,51 @@ export default function MobileMainVerticalList({ rootHeight }) {
                 <br />
                 사업자등록번호 211-88-32153 | 대표이사 구지은
               </div>
-            </div>
+            </div>*/}
+          </div>
+        </div>
+        <div className={styles.swiperSlide}>
+          <div className={styles.lastSlideBackgroundWrapper}>
+            <footer className={styles.mobileFooterBackgroundWrapper}>
+              <div>
+                <img src={kaliscoWhiteLogo} alt="kalisco logo" />
+              </div>
+              <div>
+                <div>서울시 강남구 역삼로 115 2층 (우)06243</div>
+                <div>사업자등록번호 211-88-32153 | 대표이사 구지은</div>
+              </div>
+              <div>
+                <div className={styles.mobileFooterEmailWrapper}>
+                  <div className={styles.mobileFooterEmailFlexWrapper}>
+                    <div>입점제안 및 가맹문의</div>
+                    <div>
+                      <a
+                        href="mailto:llimun@kalisco.co.kr"
+                        className={styles.mobileFooterEmailLink}
+                      >
+                        llimun@kalisco.co.kr
+                      </a>
+                    </div>
+                  </div>
+                  <div className={styles.mobileFooterEmailFlexWrapper}>
+                    <div>Easy to cook(편의식) 상품구입 및 특판문의</div>
+                    <div>
+                      <a
+                        href="mailto:kipal.cha@kalisco.co.kr"
+                        className={styles.mobileFooterEmailLink}
+                      >
+                        kipal.cha@kalisco.co.kr
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div>캘리스코 고객센터 : 1833-4550</div>
+                <div>(평일 09:00 ~ 18시, 공휴일 휴무)</div>
+              </div>
+              <div>COPYRIGHT © 2020 KALISCO. ALL RIGHTS RESERVED.</div>
+            </footer>
           </div>
         </div>
       </div>
