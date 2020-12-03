192ab7251f471f14dc1eca94e9e23f57ec91a412
Subin Lee <chandar.sblee@gmail.com>
2020-12-03 11:27:32 +0900

    공사중 컴포넌트 파일, 컴포넌트 명 변경

--- src/components/mobile/support/SupportContent.js
+++ src/components/mobile/support/SupportContent.js
@@ -3,58 +3,57 @@ import { useParams } from "react-router-dom";
 import BannerImage from "../../../static/images/mobile/img-mar-support-banner@2x.png";
 import Banner from "../banner/Banner";
 
-import SupportUnderconstruction from './SupportUnderconstruction';
-import SupportGiveaway from './SupportGiveaway';
-import SupportKakao from './SupportKakao';
-import SupportEvent from './SupportEvent';
+import SupportUnderConstruction from "./SupportUnderConstruction";
+import SupportGiveaway from "./SupportGiveaway";
+import SupportKakao from "./SupportKakao";
+import SupportEvent from "./SupportEvent";
 
 function SupportCaseRenderer({ subject }) {
-    switch (subject) {
-        case "membership":
-            return <SupportUnderconstruction />;
-        case "giveaway":
-            return <SupportGiveaway />
-        case "partnership":
-            return <SupportUnderconstruction />;
-        case "kakaotalk-friends":
-            return <SupportKakao />
-        case "events":
-            return <SupportEvent />;
-        default:
-            return <React.Fragment />;
-    }
+  switch (subject) {
+    case "membership":
+      return <SupportUnderConstruction />;
+    case "giveaway":
+      return <SupportGiveaway />;
+    case "partnership":
+      return <SupportUnderConstruction />;
+    case "kakaotalk-friends":
+      return <SupportKakao />;
+    case "events":
+      return <SupportEvent />;
+    default:
+      return <React.Fragment />;
+  }
 }
 
-
 export default function SupportContent() {
-    const { subject } = useParams();
-    const links = [
-        {
-            title: "멤버십",
-            link: "/supports/membership"
-        },
-        {
-            title: "상품권",
-            link: "/supports/giveaway"
-        },
-        {
-            title: "제휴",
-            link: "/supports/partnership"
-        },
-        {
-            title: "카카오톡 친구",
-            link: "/supports/kakaotalk-friends"
-        },
-        {
-            title: "이벤트&프로모션",
-            link: "/supports/events"
-        }
-    ];
+  const { subject } = useParams();
+  const links = [
+    {
+      title: "멤버십",
+      link: "/supports/membership",
+    },
+    {
+      title: "상품권",
+      link: "/supports/giveaway",
+    },
+    {
+      title: "제휴",
+      link: "/supports/partnership",
+    },
+    {
+      title: "카카오톡 친구",
+      link: "/supports/kakaotalk-friends",
+    },
+    {
+      title: "이벤트&프로모션",
+      link: "/supports/events",
+    },
+  ];
 
-    return (
-        <>
-            <Banner title="Support" image={BannerImage} links={links} />
-            <SupportCaseRenderer subject={subject} />
-        </>
-    );
+  return (
+    <>
+      <Banner title="Support" image={BannerImage} links={links} />
+      <SupportCaseRenderer subject={subject} />
+    </>
+  );
 }
