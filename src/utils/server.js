8e4b6e3dc213b692fbef0e114ff101a54b0022b3
Subin Lee <chandar.sblee@gmail.com>
2021-01-12 12:28:16 +0900

    실서버 배포 환경 설정

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,6 +1,6 @@
 export const url =
-  //  "https://7i5apes6bk.execute-api.ap-northeast-2.amazonaws.com/v1";
+  // "https://7i5apes6bk.execute-api.ap-northeast-2.amazonaws.com/v1";
   "https://8i95jcc1yf.execute-api.ap-northeast-2.amazonaws.com/v1/kalisco";
 
-// export const CRM_URL = "https://api.kalisco.co.kr/Homepage";
-export const CRM_URL = "http://outerpos-test.imtsoft.me";
+export const CRM_URL = "https://api.kalisco.co.kr";
+// export const CRM_URL = "http://outerpos-test.imtsoft.me";
